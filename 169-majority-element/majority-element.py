eb2f58cc67f65a0a695a1ba32b7dbcae679a5894
gentmaks <124310350+gentmaks@users.noreply.github.com>
2025-11-25 18:52:29 -0500

    Time: 7 ms (58.56%) | Memory: 19.5 MB (19.17%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,4 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return max(collections.Counter(nums).items(), key = lambda tup: tup[1])[0]
+        major_ele = nums[0]
+        maj_count = 1
+        for i in range(1, len(nums)):
+            if nums[i] != major_ele:
+                maj_count -= 1
+                if not maj_count:
+                    major_ele = nums[i]
+                    maj_count = 1
+                continue
+            maj_count += 1
+        return major_ele
         

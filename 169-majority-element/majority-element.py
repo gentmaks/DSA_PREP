4eddfc332b5aa467e7d24ce8f080c52a90e9f2ec
gentmaks <124310350+gentmaks@users.noreply.github.com>
2025-12-12 19:29:02 +0100

    Time: 8 ms (37.97%) | Memory: 19.3 MB (86.93%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq = collections.Counter(nums)
-        for num in freq:
-            if freq[num] > len(nums)//2:
-                return num
-        
+        leader = nums[0]
+        leader_count = 1     
+        for i in range(1, len(nums)):
+            if nums[i] != leader:
+                leader_count -= 1
+                if not leader_count:
+                    leader = nums[i]
+                    leader_count = 1
+            else:
+                leader_count += 1
+        return leader

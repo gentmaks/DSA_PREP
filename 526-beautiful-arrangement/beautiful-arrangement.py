bd0cb4e68a29b4dbb6471c91fdcfb64f5b7770c3
gentmaks <124310350+gentmaks@users.noreply.github.com>
2026-08-22 12:17:02 -0700

    Time: 51 ms (85.99%) | Memory: 24.2 MB (12.90%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.py
+++ 526-beautiful-arrangement/beautiful-arrangement.py
@@ -1,17 +1,32 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
+        """
+        n integers (1...n)
+        find valid perms
+            valid perm -> for each i in range (1, n):
+                perm[i] mod i === 0
+            or  i mod perm[i] === 0
+
+        so we want to check which subsets of [1...n] have those properties
+        for each (idx, number) pair in that collection
+
+        can use bitmasks to more efficiently represent each subset
+        only add numbers if condition is met, check which numbers we can add
+        when we reach the end of the subset return 1 since all conditions met
+        and this is a valid subset
+        """
         memo = {}
         def dp(mask, pos):
             if pos > n:
                 return 1
             if (mask, pos) in memo:
                 return memo[(mask, pos)]
-            res = 0
+            ans = 0
             for i in range(n):
                 num = i + 1
                 if not (mask & (1 << i)):
                     if (not num % pos) or (not pos % num):
-                        res += dp(mask | (1 << i), pos + 1)
-            memo[(mask, pos)] = res
-            return res
+                        ans += dp(mask | (1 << i), pos + 1)
+            memo[(mask, pos)] = ans
+            return ans
         return dp(0, 1)

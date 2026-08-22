86539c988f01210d003bd2e48adbf2748e76bf7b
gentmaks <124310350+gentmaks@users.noreply.github.com>
2026-08-22 13:46:49 -0700

    Time: 738 ms (19.85%) | Memory: 40.5 MB (14.64%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total = sum(nums)
+        if total % k:
+            return False
+        bucket_target = total / k
+        memo = {}
+        def dp(mask):
+            if mask == (1 << len(nums)) - 1:
+                return True
+            if mask in memo:
+                return memo[mask]
+            curr_sum = 0
+            for i in range(len(nums)):
+                if mask & (1 << i):
+                    curr_sum += nums[i]
+            curr_sum %= bucket_target
+            for i in range(len(nums)):
+                if not (mask & (1 << i)):
+                    if curr_sum + nums[i] <= bucket_target and dp(mask | (1 << i)):
+                        memo[mask] = True
+                        return True
+                    else:
+                        memo[mask] = False
+            memo[mask] = False
+            return memo[mask]
+        return dp(0)

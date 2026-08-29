c25eacec5374c4cdf97a7733fdf8a9310a890b7b
gentmaks <124310350+gentmaks@users.noreply.github.com>
2026-08-29 15:25:28 -0700

    Time: 295 ms (49.44%) | Memory: 30.6 MB (7.86%) - LeetSync

--- 821-bricks-falling-when-hit/bricks-falling-when-hit.py
+++ 821-bricks-falling-when-hit/bricks-falling-when-hit.py
@@ -2,14 +2,14 @@ class UnionFind:
     def __init__(self, n: int) -> None:
         self.parents = [i for i in range(n)]
         self.size = [1 for i in range(n)]
-    
+
     def find(self, child: int) -> int:
         if child != self.parents[child]:
             self.parents[child] = self.find(self.parents[child])
         return self.parents[child]
 
-    def union(self, node1, node2) -> bool:
-        parent1, parent2 = self.find(node1), self.find(node2)
+    def union(self, child1: int, child2: int) -> bool:
+        parent1, parent2 = self.find(child1), self.find(child2)
         if parent1 == parent2:
             return False
         if self.size[parent1] > self.size[parent2]:
@@ -19,43 +19,44 @@ class UnionFind:
             self.parents[parent1] = parent2
             self.size[parent2] += self.size[parent1]
         return True
-    
-    def get_size(self, node):
-        parent = self.find(node)
-        return self.size[parent] 
 
+    def get_size(self, child: int) -> int:
+        parent = self.find(child)
+        return self.size[parent]
+    
 class Solution:
     def hitBricks(self, grid: List[List[int]], hits: List[List[int]]) -> List[int]:
         rows = len(grid)
         cols = len(grid[0])
-        seen = set()
-        for r, c in hits:
-            if grid[r][c]:
-                seen.add((r, c))
-                grid[r][c] = 0
-
         uf = UnionFind(rows * cols + 1)
 
-        # preprocessing
+        # gathering bricks that are hit
+        store = set()
+        for r, c in hits:
+            if grid[r][c]:
+                store.add((r, c)) 
+                grid[r][c] = 0
+
+        # calculating stable component sizes in the end
         for r in range(rows):
             for c in range(cols):
                 if not r and grid[r][c]:
                     uf.union(c, rows * cols)
                 if grid[r][c]:
-                    for rr, cc in (r-1, c), (r, c-1):
+                    for rr, cc in [(r - 1, c), (r, c - 1)]:
                         if rr < 0 or rr >= rows or cc < 0 or cc >= cols or not grid[rr][cc]:
                             continue
-                        uf.union(r* cols + c, rr * cols + cc)
+                        uf.union(r * cols + c, rr * cols + cc)
 
-        # number of bricks each turn
+        # calculating falling bricks each hit
         res = []
         prev = uf.get_size(rows * cols)
         for r, c in reversed(hits):
-            if (r, c) in seen:
+            if (r, c) in store:
                 grid[r][c] = 1
                 if not r:
                     uf.union(c, rows * cols)
-                for rr, cc in (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1):
+                for rr, cc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
                     if rr < 0 or rr >= rows or cc < 0 or cc >= cols or not grid[rr][cc]:
                         continue
                     uf.union(r * cols + c, rr * cols + cc)

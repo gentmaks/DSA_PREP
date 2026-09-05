3fd6b34dee97e0b09f28675272c15fe692f3389a
gentmaks <124310350+gentmaks@users.noreply.github.com>
2026-09-05 00:46:32 -0700

    Time: 1117 ms (5.17%) | Memory: 352.3 MB (5.17%) - LeetSync

---
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        digits = [int(bit) for bit in s]
+        size = len(digits)
+        memo = {}
+        def dp(i, prev):
+            if i == size:
+                return 0
+            key = (i, prev)
+            if key in memo:
+                return memo[key]
+            # case 1 is keep current digit as is
+            keep = (digits[i] != prev) + dp(i + 1, prev)
+            # case 2 is switch the current digit
+            switch = float("inf")
+            if not prev:
+                switch = (digits[i] != 1) + dp(i + 1, 1)
+            memo[key] = min(keep, switch)
+            return memo[key]
+        return dp(0, 0)

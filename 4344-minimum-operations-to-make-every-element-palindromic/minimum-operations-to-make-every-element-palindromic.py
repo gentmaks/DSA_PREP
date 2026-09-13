31f1e3228a0523fb5710c229f1d159c55febeac7
gentmaks <124310350+gentmaks@users.noreply.github.com>
2026-09-13 13:37:53 -0700

    Time: 359 ms (60.00%) | Memory: 42.1 MB (0.00%) - LeetSync

--- 4344-minimum-operations-to-make-every-element-palindromic/minimum-operations-to-make-every-element-palindromic.py
+++ 4344-minimum-operations-to-make-every-element-palindromic/minimum-operations-to-make-every-element-palindromic.py
@@ -1,20 +1,20 @@
+bound = 10 ** 9
+parity = [[], []]
+for i in range(1, 100000):
+    s = str(i)
+    s_odd = s[:-1] + s[::-1]
+    s_even = s + s[::-1]
+    v_odd = int(s_odd)
+    v_even = int(s_even)
+    if v_odd < bound: parity[v_odd & 1].append(v_odd)
+    if v_even < bound: parity[v_even & 1].append(v_even)
+parity[0].sort()
+parity[1].sort()
 class Solution:
-    bound = 10 ** 9
-    parity = [[], []]
-    for i in range(1, 100000):
-        s = str(i)
-        s_odd = s[:-1] + s[::-1]
-        s_even = s + s[::-1]
-        v_odd = int(s_odd)
-        v_even = int(s_even)
-        if v_odd < bound: parity[v_odd & 1].append(v_odd)
-        if v_even < bound: parity[v_even & 1].append(v_even)
-    parity[0].sort()
-    parity[1].sort()
     def minOperations(self, nums: list[int]) -> int:
         tally = 0
         for num in nums:
-            space = self.parity[num & 1]
+            space = parity[num & 1]
             i = bisect_left(space, num)
             if i >= len(space):
                 i = len(space) - 1
